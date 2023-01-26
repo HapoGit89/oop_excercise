35e83aeac0241a2691cdc24a16957f7a9641e2ed
Hannes <hannes.porombka@gmail.com>
2023-01-26 15:36:03 +0100

    finished part 4

--- oop.js
+++ oop.js
@@ -31,9 +31,30 @@ class Motorcycle extends Car {
     }
 }
 
+class Garage {
+    constructor(cap){
+        this.cap = cap
+        this.vehicles = []
+
+    }
+    add(vehicle){
+        if(this.cap <= this.vehicles.length){
+            console.log("Sorry we`re full!")
+        }else if (!(vehicle instanceof Vehicle)){
+            console.log("Sorry only vehicles allowed")
+        }else {
+            this.vehicles.push(vehicle)
+        }
+
+    }
+}
+
 let test = new Vehicle("Honda", "Monster Truck", 1999);
 
 let myFirstCar = new Car("Honda", "Monster Truck", 1999);
 
-
 let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+let mySecondMotorcycle = new Motorcycle("Hondaaa", "Nighthawk", 2000);
+
+let garage = new Garage (3);
